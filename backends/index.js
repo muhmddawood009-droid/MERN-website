32badb11b6d8d6352fe852b32866c63a43ac9156
muhmddawood009-droid <muhmd.dawood009@gmail.com>
2026-04-22 05:14:57 +0500

    url changed to vercel urls

--- backends/index.js
+++ backends/index.js
@@ -22,7 +22,7 @@ connectCloudinary();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://mern-website-frontend-eight.vercel.app",
   credentials: true,
 }));
 app.use(cookieParser());
